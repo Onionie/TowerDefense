747148405d89abbaa793c71438b4098de955d724
Onionie <gutierrez.onionie@gmail.com>
2021-03-08 21:53:51 -0800

    Working Currency and Upgrade

--- Assets/Scripts/CurrencySystem.cs
+++ Assets/Scripts/CurrencySystem.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CurrencySystem : MonoBehaviour
+public class CurrencySystem : Singleton<CurrencySystem>
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int coinTest;
+    private string CURRENCY_SAVE_KEY = "MYGAME_CURRENCY";
+
+    public int TotalCoins { get; set; }
+
+    private void Start()
     {
-        
+        PlayerPrefs.DeleteKey(CURRENCY_SAVE_KEY);
+        LoadCoins();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LoadCoins()
     {
-        
+        TotalCoins = PlayerPrefs.GetInt(CURRENCY_SAVE_KEY, coinTest);
+    }
+
+    public void AddCoins(int amount)
+    {
+        TotalCoins += amount;
+        PlayerPrefs.SetInt(CURRENCY_SAVE_KEY, TotalCoins);
+        PlayerPrefs.Save();
+    }
+
+    public void RemoveCoins(int amount)
+    {
+        if (TotalCoins >= amount)
+        {
+            TotalCoins -= amount;
+            PlayerPrefs.SetInt(CURRENCY_SAVE_KEY, TotalCoins);
+            PlayerPrefs.Save();
+        }
+    }
+
+    private void AddCoins(Enemy enemy)
+    {
+        AddCoins(1);
+    }
+
+    private void OnEnable()
+    {
+        EnemyHealth.OnEnemyKilled += AddCoins;
+    }
+
+    private void OnDisable()
+    {
+        EnemyHealth.OnEnemyKilled -= AddCoins;
     }
 }

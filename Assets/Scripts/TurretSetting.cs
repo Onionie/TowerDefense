500d547771bb9c9bb7419775274fd496a46616cd
Onionie <gutierrez.onionie@gmail.com>
2021-03-09 21:52:26 -0800

    Created Turret Panel

---
+++ Assets/Scripts/TurretSetting.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Turret Shop Setting")]
+public class TurretSetting : ScriptableObject
+{
+    public GameObject TurretPrefab;
+    public int TurretShopCost;
+    public Sprite TurretShopSprite;
+
+}

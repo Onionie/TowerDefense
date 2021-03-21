a26c83d58dea8811fc411b6af8a6826abc4cdd29
Onionie <gutierrez.onionie@gmail.com>
2021-03-21 00:17:05 -0700

    Achievement Update

---
+++ Assets/Scripts/AchievementCard.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementCard : MonoBehaviour
+{
+    [SerializeField] private Image achievementImage;
+    [SerializeField] private TextMeshProUGUI title;
+    [SerializeField] private TextMeshProUGUI progress;
+    [SerializeField] private TextMeshProUGUI reward;
+  //  [SerializeField] private Button rewardButton;
+
+    public void SetupAchievement(Achievement achievement)
+    {
+       // AchievementLoaded = achievement;
+        achievementImage.sprite = achievement.Sprite;
+        title.text = achievement.Title;
+        //progress.text = achievement.GetProgress();
+        reward.text = achievement.GoldReward.ToString();
+    }
+}

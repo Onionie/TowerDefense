b5a4e49b76a1ba782090c11234619d24bf020e34
Onionie <gutierrez.onionie@gmail.com>
2021-03-26 21:33:13 -0700

    Wave Update

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -20,8 +20,8 @@ public class Spawner : MonoBehaviour
     [SerializeField] private SpawnModes spawnMode = SpawnModes.Fixed;
     [SerializeField] private int enemyCount = 10;
     [SerializeField] private float delayBtwWaves = 1f;
-    
-    
+
+
     [Header("Fixed Delay")]
     [SerializeField] private float delayBetwSpawns;
 
@@ -29,17 +29,24 @@ public class Spawner : MonoBehaviour
     [SerializeField] private float minRandomDelay;
     [SerializeField] private float maxRandomDelay;
 
+    [Header("Poolers")]
+    [SerializeField] private ObjectPooler enemyWave1To10Pooler;
+    [SerializeField] private ObjectPooler enemyWave11To20Pooler;
+    [SerializeField] private ObjectPooler enemyWave21To30Pooler;
+    [SerializeField] private ObjectPooler enemyWave31To40Pooler;
+    [SerializeField] private ObjectPooler enemyWave41To50Pooler;
+
+
+
     private float spawnTimer;
     private int enemiesSpawned;
     private int enemiesRem;
 
-    private ObjectPooler pooler;
     private Waypoint waypoint;
 
 
     private void Start()
     {
-        pooler = GetComponent<ObjectPooler>();
         waypoint = GetComponent<Waypoint>();
 
         enemiesRem = enemyCount;
@@ -63,7 +70,7 @@ public class Spawner : MonoBehaviour
 
     private void SpawnEnemy()
     {
-        GameObject newInstance = pooler.GetInstanceFromPool();
+        GameObject newInstance = GetPooler().GetInstanceFromPool();
         Enemy enemy = newInstance.GetComponent<Enemy>();
         enemy.Waypoint = waypoint;
         enemy.ResetEnemy();
@@ -91,6 +98,37 @@ public class Spawner : MonoBehaviour
     {
         float randomTimer = Random.Range(minRandomDelay, maxRandomDelay);
         return randomTimer;
+    }
+
+    private ObjectPooler GetPooler()
+    {
+        int currentWave = LevelManager.Instance.CurrentWave;
+        if (currentWave <= 10) // 1- 10
+        {
+            return enemyWave1To10Pooler;
+        }
+
+        if (currentWave > 10 && currentWave <= 20) // 11- 20
+        {
+            return enemyWave11To20Pooler;
+        }
+
+        if (currentWave > 20 && currentWave <= 30) // 21- 30
+        {
+            return enemyWave21To30Pooler;
+        }
+
+        if (currentWave > 30 && currentWave <= 40) // 31- 40
+        {
+            return enemyWave31To40Pooler;
+        }
+
+        if (currentWave > 40 && currentWave <= 50) // 41- 50
+        {
+            return enemyWave41To50Pooler;
+        }
+
+        return null;
     }
 
     private IEnumerator NextWave()
